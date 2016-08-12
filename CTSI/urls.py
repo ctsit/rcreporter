20dcb16ecef6bc20ae2054ebfb0999296820d2eb
Ashish <nautiyal-ashish@live.com>
2016-08-12 11:10:57 -0400

    Modified the main app url to be root

--- CTSI/urls.py
+++ CTSI/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^report/', include('reportingTool.urls')),
+    url(r'^', include('reportingTool.urls', namespace='reportingTool')),
     url(r'^admin/', admin.site.urls),
 ]
