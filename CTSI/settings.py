d97d3ab102bd1dcf5a165f5d9de36baa26f1e413
Ashish <nautiyal-ashish@live.com>
2016-08-12 11:10:30 -0400

    Add static files section to where css files would be stored

--- CTSI/settings.py
+++ CTSI/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_filters',
+    'bootstrap_toolkit',
     'django_tables2',
     'reportingTool.apps.ReportingtoolConfig',
     'django.contrib.admin',
