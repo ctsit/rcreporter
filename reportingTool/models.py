1dc9f115439c4302ce91141c6180d0f0bb515c80
ash-kai <nautiyal-ashish@live.com>
2016-08-19 04:30:37 -0400

    Added docStrings for reporting tool modules

--- reportingTool/models.py
+++ reportingTool/models.py
@@ -1,5 +1,8 @@
-from __future__ import unicode_literals
+"""
+This module contais the module description for this Django Project.
+"""
 
+from __future__ import unicode_literals
 from django.db import models
 from datetime import date
 
@@ -8,6 +11,10 @@ from datetime import date
 # Create your models here.
 
 class Projects(models.Model):
+	"""
+	Model for Projects table.
+	"""
+
 	projectID = models.AutoField(primary_key=True, verbose_name='Project ID')
 	projectName = models.CharField(max_length=1000, verbose_name='Project Name')
 
@@ -18,6 +25,10 @@ class Projects(models.Model):
 		return self.projectName
 
 class Sites(models.Model):
+	"""
+	Model for Sites table.
+	"""
+
 	siteID = models.AutoField(primary_key=True, verbose_name='Site ID')
 	siteName = models.CharField(max_length=1000, verbose_name='Site Name')
 	project_ID = models.ForeignKey(Projects, db_column='project_ID', verbose_name='Project')
@@ -29,6 +40,10 @@ class Sites(models.Model):
 		return self.siteName
 
 class Proj_Exec_TimeStmp(models.Model):
+	"""
+	Model for Proj_Exec_TimeStmp table.
+	"""
+
 	timeStmpID = models.AutoField(primary_key=True, verbose_name='TimeStamp ID')
 	startTimeStmp = models.DateTimeField(verbose_name='Start TimeStamp')
 	endTimeStmp = models.DateTimeField(verbose_name='End TimeStamp')
@@ -38,9 +53,13 @@ class Proj_Exec_TimeStmp(models.Model):
 		db_table = "PROJ_EXEC_TIMESTMP"
 
 	def __unicode__(self):
-		return str(self.startTimeStmp.strftime("%m/%d/%Y %I:%M %a")) + ' - ' + str(self.endTimeStmp.strftime("%Y-%m-%d %I:%M %p"))
+		return str(self.startTimeStmp.strftime("%m/%d/%Y")) + ' - ' + str(self.endTimeStmp.strftime("%m/%d/%Y"))
 
 class Site_Reports(models.Model):
+	"""
+	Model for Site_Reports table.
+	"""
+
 	reportID = models.AutoField(primary_key=True, verbose_name='Report ID')
 	patientCount = models.IntegerField(verbose_name='Patient Count')
 	exec_ID = models.ForeignKey(Proj_Exec_TimeStmp, db_column='exec_ID', verbose_name='TimeStamp ID')
@@ -53,6 +72,10 @@ class Site_Reports(models.Model):
 		return str(self.reportID)
 
 class Queries(models.Model):
+	"""
+	Model for Queries table.
+	"""
+
     query_text = models.CharField(verbose_name='Query Text', max_length=1000)
     query_sql = models.CharField(verbose_name='Query SQL', max_length=1000)
     pub_date = models.DateField(verbose_name='Published Date', default=date.today)
--- reportingTool/tables.py
+++ reportingTool/tables.py
@@ -1,24 +1,36 @@
+"""
+Module to implement table functionality for all the models.
+
+This module uses django_tables2 to implement the tables.
+The use of default django_tables2 enables to generate table including all
+the fields as column for the give model.
+"""
+
 import django_tables2 as tables
 from .models import Projects, Sites, Site_Reports, Proj_Exec_TimeStmp, Queries
 
 class ProjectsTable(tables.Table):
+	"""Implement table over Projects model. """
 	class Meta:
 		model = Projects
 
 class SitesTable(tables.Table):
+	"""Implement table over Sites model. """
 	class Meta:
 		model = Sites
 
 class ProjExecTable(tables.Table):
+	"""Implement table over Proj_Exec_TimeStmp model. """
 	class Meta:
 		model = Proj_Exec_TimeStmp
 
 class SiteReportsTable(tables.Table):
+	"""Implement table over Site_Reports model. """
 	class Meta:
 		model = Site_Reports
 
 class QueriesTable(tables.Table):
+	"""Implement table over Queries model. """
 	class Meta:
 		model = Queries
-		# add class="paleblue" to <table> tag
 		
